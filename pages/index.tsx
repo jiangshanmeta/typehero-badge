a4bac321a5b8ff8bad1a3c2a7c9a5f2f22941e5e
jiangshanmeta <540118044@qq.com>
2024-11-26 12:39:37 +0800

    feat: icon

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,7 +14,7 @@ export default function Home() {
                 <title>TypeHero Badge</title>
                 <meta name="description" content="TypeHero Badge" />
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
-                <link rel="icon" href="/favicon.ico" />
+                <link rel="icon" href="/caixukun.svg" />
             </Head>
             <Box
                 style={{
